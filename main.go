1ed688d8a156ac3422bbbac03ceedf0ea5e344b8
Roman Kulkov <romawka2410@gmail.com>
2023-11-22 13:35:23 +0200

    functions

--- main.go
+++ main.go
@@ -6,9 +6,36 @@ import "fmt"
 
 // functions
 
+type Animal struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+}
+
+func (a *Animal) says() {
+	fmt.Printf("A %s says %s", a.Name, a.Sound)
+	fmt.Println()
+}
+func (a *Animal) HowManyLegs() {
+	fmt.Printf("A %s has %d legs", a.Name, a.NumberOfLegs)
+	fmt.Println()
+}
+
 func main() {
-	myTotal := sumMany(2, 6, 7, 9, -5)
-	fmt.Println(myTotal)
+	var dog Animal
+	dog.Name = "dog"
+	dog.Sound = "gaf"
+	dog.NumberOfLegs = 4
+	dog.says()
+
+	cat := Animal{
+		Name:         "cat",
+		Sound:        "meow",
+		NumberOfLegs: 4,
+	}
+	cat.says()
+	cat.HowManyLegs()
+
 }
 func sumMany(nums ...int) int {
 	total := 0
