a589d6be3a782ec03a76c62ba5579937052fa6a5
Roman Kulkov <romawka2410@gmail.com>
2023-11-22 13:02:35 +0200

    map

--- main.go
+++ main.go
@@ -1,57 +1,34 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
 // reference types - типи посилань (pointers -вказівник, slices, maps, functions, channels)
 
-// slices -вказівник (specific location in memory)
+// map
+
 func main() {
-	var animals []string
-	animals = append(animals, "dog")
-	animals = append(animals, "fish")
-	animals = append(animals, "cat")
-	animals = append(animals, "horse")
+	intMap := make(map[string]int)
 
-	fmt.Println(animals)
+	intMap["one"] = 1
+	intMap["two"] = 2
+	intMap["three"] = 3
+	intMap["four"] = 4
+	intMap["five"] = 5
 
-	for i, x := range animals {
-		//i - index in the slise,
-		//x - curent item in the slise that i`m looking at(поточний елемент на слайді, який я переглядаю)
-		fmt.Println(i, x)
+	for key, value := range intMap {
+		fmt.Println(key, value)
 	}
-	fmt.Println("Element 0 is", animals[0])
 
-	fmt.Println("First two elements are", animals[0:2])
+	//delete(intMap, "four")
 
-	fmt.Println("The slice is", len(animals), "elements long")
+	el, ok := intMap["four"]
+	if ok {
+		fmt.Println(el, "is in map")
+	} else {
+		fmt.Println(el, "is in not map")
+	}
 
-	fmt.Println("Is it sorted?", sort.StringsAreSorted(animals))
+	intMap["two"] = 4
+	fmt.Println(intMap)
 
-	sort.Strings(animals)
-
-	fmt.Println("Is it sorted now?", sort.StringsAreSorted(animals))
-	fmt.Println(animals)
-
-	animals = DeleteFromSlice(animals, 1)
-	fmt.Println(animals)
-	fmt.Println("The slice is", len(animals), "elements long now")
-
-}
-func DeleteFromSlice(a []string, i int) []string {
-	a[i] = a[len(a)-1] // we copy last element to index i
-	a[len(a)-1] = ""
-
-	//then we erase the last element by giving it its default empty value which for a string is an empty string
-	//потім ми стираємо останній елемент, надавши йому порожнє значення за замовчуванням, яке для рядка є порожнім рядком
-	//and then we simply truncate the slise by deleting the last element
-	// а потім ми просто обрізаємо slise, видаливши останній елемент
-
-	a = a[:len(a)-1]
-	// is len "a" will be the length of the slice minus 1 which is the last thing, so we`re returning
-	// довжина "a" буде довжиною slise мінус 1, що є останнім, тому ми повертаємося
-
-	return a
 }
