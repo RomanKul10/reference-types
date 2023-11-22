8595fac74bbadb205ff612d0f1b37312fbc2cd33
Roman Kulkov <romawka2410@gmail.com>
2023-11-22 14:10:26 +0200

    channels

--- main.go
+++ main.go
@@ -1,47 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/eiannone/keyboard"
+)
 
 // reference types - типи посилань (pointers -вказівник, slices, maps, functions, channels)
 
-// functions
-
-type Animal struct {
-	Name         string
-	Sound        string
-	NumberOfLegs int
-}
-
-func (a *Animal) says() {
-	fmt.Printf("A %s says %s", a.Name, a.Sound)
-	fmt.Println()
-}
-func (a *Animal) HowManyLegs() {
-	fmt.Printf("A %s has %d legs", a.Name, a.NumberOfLegs)
-	fmt.Println()
-}
+// channels
+var keyPressChan chan rune
 
 func main() {
-	var dog Animal
-	dog.Name = "dog"
-	dog.Sound = "gaf"
-	dog.NumberOfLegs = 4
-	dog.says()
+	keyPressChan = make(chan rune)
 
-	cat := Animal{
-		Name:         "cat",
-		Sound:        "meow",
-		NumberOfLegs: 4,
+	go listenForKeyPress()
+
+	fmt.Println("Press any key, or q to quit")
+	_ = keyboard.Open()
+	defer func() {
+		keyboard.Close()
+	}()
+
+	for {
+		char, _, _ := keyboard.GetSingleKey()
+		if char == 'q' || char == 'Q' {
+			break
+		}
+
+		keyPressChan <- char
 	}
-	cat.says()
-	cat.HowManyLegs()
-
 }
-func sumMany(nums ...int) int {
-	total := 0
 
-	for _, x := range nums {
-		total = total + x
+func listenForKeyPress() {
+	for {
+		key := <-keyPressChan
+		fmt.Println("You pressed", string(key))
 	}
-	return total
 }
